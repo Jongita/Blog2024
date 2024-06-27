ea55643b2a07f76a4241daf2f2d69fa53b859868
Jongita <jurgitajonaityte@gmail.com>
2024-06-27 07:49:06 +0300

    Initial

---
+++ server/src/app.ts
@@ -0,0 +1,26 @@
+import { error } from "console";
+import express, { Application } from "express";
+import mongoose from "mongoose";
+import { Post } from "../model/post";
+
+
+
+const app:Application=express();
+
+// mongodb://localhost:27017/
+
+mongoose.connect("mongodb://localhost:27017/blog")
+    .then(()=>{
+        console.log("Prisijungėme prie MongoDB");
+        const naujasIrasas=new Post({
+            title:"Antras įrašas",
+            content:"Antro įrašo tekstas"
+        });
+        console.log(naujasIrasas);
+        naujasIrasas.save();
+
+    })
+    .catch((error)=>console.log(error));
+
+
+export {app};
---
+++ server/src/server.ts
@@ -0,0 +1,6 @@
+import { app } from "./app";
+
+
+app.listen(5999, ()=>{
+ console.log("Serveris paleistas");
+});
